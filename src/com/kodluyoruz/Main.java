180aad1346096c4f1be350fc407bbf777c28ad57
tugbayavuzz <tugbayavuz@yandex.com>
2021-01-09 16:17:06 +0300

    Java101

--- src/com/kodluyoruz/Main.java
+++ src/com/kodluyoruz/Main.java
@@ -5,8 +5,8 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        int sayi1 =700; //32 bit
-        Integer sayiA =300;
+        int sayi1 = 700; //32 bit
+        Integer sayiA = 300;
 
         System.out.println(sayi1);
         System.out.println(sayiA);//sayiA ile 300 değerinin  tutulduğu yeri göster ve referans olan değeri göster
@@ -22,18 +22,18 @@ public class Main {
 
 
         System.out.println(
-                sayi1   +"\n"
-                + sayi2 +"\n"
-                + sayi3 +"\n"
-                + sayi4 +"\n"
-                + ondalikliSayi +"\n"
-                + ondalikliSayi2 +"\n"
-                + harf +"\n"
-                + harfler +"\n"
-                + harfVarmi);
+                sayi1 + "\n"
+                        + sayi2 + "\n"
+                        + sayi3 + "\n"
+                        + sayi4 + "\n"
+                        + ondalikliSayi + "\n"
+                        + ondalikliSayi2 + "\n"
+                        + harf + "\n"
+                        + harfler + "\n"
+                        + harfVarmi);
 
         //#################################################################################################
-        for (int i =0 ; i<=10; i++){ //0 dan 10 a kadar olan sayıları ekrana yazdırma
+        for (int i = 0; i <= 10; i++) { //0 dan 10 a kadar olan sayıları ekrana yazdırma
             System.out.println(i);
         }
         //#################################################################################################
@@ -45,27 +45,49 @@ public class Main {
         System.out.println(meyveler); //eklenen meyve isimleri yazdırıldı
 
 
-
         //#################################################################################################
         //diğer bir for kullanımıyla dizinin yazdırılması
-        for (String elements : meyveler){
+        for (String elements : meyveler) {
             System.out.println(elements);
         }
 
 
         //#################################################################################################
-        String[] meyveler2 = {"elma", "armut","erik"};//diğer bir String dizisi kullanım
-        int sayac1=0,sayac2=0;
+        String[] meyveler2 = {"elma", "armut", "erik"};//diğer bir String dizisi kullanım
+        int sayac1 = 0, sayac2 = 0;
 
-        for (String elements : meyveler2){
+        for (String elements : meyveler2) {
             System.out.println(elements);
-            System.out.println("sayac: "+ sayac1++);
-            System.out.println("2.sayac: "+ ++sayac2);
+            System.out.println("sayac: " + sayac1++);
+            System.out.println("2.sayac: " + ++sayac2);
             System.out.println("---------------------");
         }
 
 
+        //#################################################################################################
+        //IF Bloğu sql deki where gibi diyebiliriz.Asal olup  olmadığını kontrol eden kod
+        int kalan;
+        boolean asalMi = true;
+        int sayi = 1;
+        if (sayi == 1) {
+            asalMi = false;
+        } else {
+            for (int i = 2; i <= sayi / 2; i++) {
+                kalan = sayi % i;
+                if (kalan == 0 || sayi == 1) {
+                    asalMi = false;
+                    break;
+                }
+            }
+        }
 
+        if (asalMi) {
+            System.out.println(sayi + " asal bir sayıdır");
+        } else
+            System.out.println(sayi + " asal bir sayı değildir");
 
     }
+
+
 }
+
