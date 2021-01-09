53db9213403ff4af50d2059047152beeb486b895
tugbayavuzz <tugbayavuz@yandex.com>
2021-01-09 15:35:18 +0300

    Java101

--- src/com/kodluyoruz/Main.java
+++ src/com/kodluyoruz/Main.java
@@ -1,9 +1,16 @@
 package com.kodluyoruz;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
         int sayi1 =700; //32 bit
+        Integer sayiA =300;
+
+        System.out.println(sayi1);
+        System.out.println(sayiA);//sayiA ile 300 değerinin  tutulduğu yeri göster ve referans olan değeri göster
+
         long sayi2 = 19983; //64 bit
         short sayi3 = 23;//16 bit
         byte sayi4 = 10;//8 bit
@@ -13,8 +20,7 @@ public class Main {
         String harfler = "Merhaba Kodluyoruz";
         boolean harfVarmi = false;
 
-        //References Types
-        //Classes, Interfaces,Arrays
+
         System.out.println(
                 sayi1   +"\n"
                 + sayi2 +"\n"
@@ -25,5 +31,28 @@ public class Main {
                 + harf +"\n"
                 + harfler +"\n"
                 + harfVarmi);
+
+
+        for (int i =0 ; i<=10; i++){ //0 dan 10 a kadar olan sayıları ekrana yazdırma
+            System.out.println(i);
+        }
+
+        //array liste sonradan eleman ekleyebiliyroum ve bu yüzden kesin kayıt sayısı belli olmayanlar  için kullanılır
+        ArrayList<String> meyveler = new ArrayList<>();
+        meyveler.add("elma"); //elma meyveler dizisine eklendi
+        meyveler.add("portakal");
+        meyveler.add("mandalina");
+        System.out.println(meyveler); //eklenen meyve isimleri yazdırıldı
+
+        //diğer bir for kullanımıyla dizinin yazdırılması
+        for (String elements : meyveler){
+            System.out.println(elements);
+        }
+
+        String[] meyveler2 = {"elma", "armut","erik"};//diğer bir String dizisi kullanım
+        for (String elements : meyveler2){
+            System.out.println(elements);
+        }
+
     }
 }
