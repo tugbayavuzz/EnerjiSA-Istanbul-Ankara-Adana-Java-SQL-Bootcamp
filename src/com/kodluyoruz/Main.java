aee13a701a32ff19a416b314e34992cc700b2bcb
tugbayavuzz <tugbayavuz@yandex.com>
2021-01-09 14:34:56 +0300

    first commit

---
+++ src/com/kodluyoruz/Main.java
@@ -0,0 +1,20 @@
+package com.kodluyoruz;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int sayi1 =700; //32 bit
+        long sayi2 = 19983; //64 bit
+        short sayi3 = 23;//16 bit
+        byte sayi4 = 10;//8 bit
+        float ondalikliSayi = 14.5F;//32 bit
+        double ondalikliSayi2 = 14.5;//64 bit
+        char harf = 'a';//16 bit;
+        String harfler = "Merhaba Kodluyoruz";
+        boolean harfVarmi = false;
+
+        //References Types
+        //Classes, Interfaces,Arrays
+        System.out.println("Hello " + args[0] + " " + args[1]);
+    }
+}
