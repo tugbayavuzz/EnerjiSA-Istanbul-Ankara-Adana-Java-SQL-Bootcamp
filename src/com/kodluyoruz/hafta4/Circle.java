b3e683022d3875bc89c60e3b0a157cc00d8f909c
tugbayavuzz <tugbayavuz@yandex.com>
2021-01-26 22:21:45 +0300

    Circle calculated

--- src/com/kodluyoruz/hafta4/Circle.java
+++ src/com/kodluyoruz/hafta4/Circle.java
@@ -1,9 +1,12 @@
 package com.kodluyoruz.hafta4;
 
+import static java.lang.Math.PI;
+
 public class Circle {
     //genellikle dışarıdan erişilmesini istemediğimiz için private yaptık
     private double radius;
     private String color; //private diyerek encapsulationdır aslında bu
+    double area;
 
     //constructorları tanımlarken sınıfın kedni ismiyle aynı olmalı
     //cunku o sınıfın varsayılan degerlerini temsil ediyor.
@@ -23,6 +26,24 @@ public class Circle {
         color = "green";
     }
 
+    //yaricapi getirmek istiyoruz
+    public double getRadius() {
+        return radius;
+    }
 
+    //degerinin disaridan degistirilebilir olmasını istersek setlemek gerekir
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
 
+    public String getColor() {
+        return color;
+    }
+
+    public double getArea() {
+        return radius * radius * PI;
+    }
+    public double getArea(double r){
+        return r*r*PI;
+    }
 }
--- src/com/kodluyoruz/hafta4/MainCircle.java
+++ src/com/kodluyoruz/hafta4/MainCircle.java
@@ -6,5 +6,10 @@ public class MainCircle {
         Circle object1 = new Circle(); //circle da olan radius ve lila özelliklerine sahip olmus oldu
         Circle object2 = new Circle(4.0, "red");
         Circle object3 = new Circle(5.0);
+
+        double object = object1.getRadius();
+        System.out.println("1.Object Radius: "+object);
+        System.out.println("1.Object Color: "+object1.getColor());
+        System.out.println("1.Object Area: "+ object1.getArea());
     }
 }
